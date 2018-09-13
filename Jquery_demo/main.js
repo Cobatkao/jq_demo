c187320ebef13589a1b1b1426bc3faab795e4931
gaohang <isaackao@foxmail.com>
2018-09-13 17:23:03 +0800

    jq_ajax_demo

--- Jquery_demo/main.js
+++ Jquery_demo/main.js
@@ -2,6 +2,11 @@ let trigger = $('#trigger')
 let board = $('#board')
 let loaded = false
 
+let form = $('#search')
+let input = $('input#username')
+let reuslt = $('#result')
+let username;
+
 trigger.on('click', () => {
   if (board.is(':visible')) {
     board.slideUp()
@@ -16,7 +21,35 @@ trigger.on('click', () => {
     }
   }
 })
+
+form.on('submit', (e) => {
+  e.preventDefault()
+  username = input.val()
+  $.ajax('https://api.github.com/users/' + username)
+    .done(((data) => {
+      let html = 
+        "<div>用户名：" + data.login + "</div>" +
+        "<div>介绍：" + (data.bio || '这用户很懒，什么都木有填') + "</div>"
+      reuslt.html(html)
+    }))
+})
   
+// 常规用法
+// $.ajax('https://api.github.com/users/CobatKao', {
+//   method: 'get',
+//   data: {
+//     username: 'whh',
+//     password: 'asdf'
+//   },
+//   success: function() {
+//     // 成功回调
+//     console.log(成功)
+//   },
+//   error: function() {
+//     // 失败回调
+//     console.log(失败)
+//   }
+// })
   
   
   
